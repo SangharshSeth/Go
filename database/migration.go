8fbffa20d85424dc474438dc0a02a44c7374055b
Sangharsh Seth <sangharsh@autovert.in>
2023-04-26 19:50:28 +0530

    models working

--- database/migration.go
+++ database/migration.go
@@ -5,7 +5,7 @@ import "CLI-Tools/models"
 func Migrate() {
 	ConnectDatabase()
 	db := GetDatabase()
-	err := db.AutoMigrate(&models.User{})
+	err := db.AutoMigrate(&models.User{}, &models.UserProfile{})
 	if err != nil {
 		panic(err)
 	}
