7782521fdd346ce7749c2917e1fecfdca64e84b2
Sangharsh Seth <sangharsh@autovert.in>
2023-05-05 04:21:20 +0530

    whatever changes plz work

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"CLI-Tools/database"
 	"CLI-Tools/routes"
+	"fmt"
 	"net/http"
 )
 
@@ -21,11 +22,19 @@ func main() {
 	AuthHandler := routes.AuthenticationHandler{}
 	LogHandler := routes.MediaHandler{}
 
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fprint, err := fmt.Fprint(writer, "Hello From Port 8080")
+		if err != nil {
+			return
+		}
+		_ = fprint
+		return
+	})
 	mux.Handle("/auth/", &AuthHandler)
 	mux.Handle("/upload", &FileUploadHandler)
 	mux.HandleFunc("/video/", LogHandler.ServeHTTP)
 
-	err := http.ListenAndServe(":443", mux)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
 	}
