131977be8154c33af1e1ac10e0302ff7b093acaf
Sangharsh Seth <sethsangharsh@outlook.com>
2023-07-16 02:07:51 +0530

    Authentication Service updates

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/sangharshseth/routes"
 	"log"
 	"net/http"
@@ -10,24 +9,13 @@ import (
 
 func main() {
 
-	//conn := database.GetDatabase()
-
-	//database.Migrate()
-	//database.Migrate()
-
 	mux := http.NewServeMux()
 
 	FileUploadHandler := routes.HttpHandler{}
 	AuthHandler := routes.AuthenticationHandler{}
 	LogHandler := routes.MediaHandler{}
 
-	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fprint, err := fmt.Fprint(writer, "Hello From Port 8080")
-		if err != nil {
-			return
-		}
-		_ = fprint
-	})
+	//Routes
 	mux.Handle("/auth/", &AuthHandler)
 	mux.Handle("/upload", &FileUploadHandler)
 	mux.HandleFunc("/video/", LogHandler.ServeHTTP)
